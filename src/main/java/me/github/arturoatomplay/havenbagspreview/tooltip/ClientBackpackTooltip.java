454a23ff8aa8ad293c0933b371a133e5a94dac13
ArturoAtomplay <64356325+ArturoAtomplay@users.noreply.github.com>
2026-01-28 20:34:18 -0600

    Update to Minecraft 1.21.6-1.21.8

--- src/main/java/me/github/arturoatomplay/havenbagspreview/tooltip/ClientBackpackTooltip.java
+++ src/main/java/me/github/arturoatomplay/havenbagspreview/tooltip/ClientBackpackTooltip.java
@@ -3,7 +3,7 @@ package me.github.arturoatomplay.havenbagspreview.tooltip;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -81,7 +81,7 @@ public class ClientBackpackTooltip implements ClientTooltipComponent {
     }
 
     private void blit(GuiGraphics guiGraphics, int x, int y, Texture texture) {
-        guiGraphics.blit(RenderType::guiTextured, TEXTURE_LOCATION, x, y, (float)texture.x, (float)texture.y, texture.w, texture.h, 128, 128);
+        guiGraphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE_LOCATION, x, y, (float)texture.x, (float)texture.y, texture.w, texture.h, 128, 128);
     }
 
     private int gridSizeX() {
